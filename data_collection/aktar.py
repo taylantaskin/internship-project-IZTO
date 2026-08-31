d8d7cf43dbad1aa4a93b425a48e806f9133713fb
Taylan Taskin <taylantaskin@yahoo.com>
2026-08-31 10:47:36 +0300

    Implement structural updates and optimizations across multiple modules

--- data_collection/aktar.py
+++ data_collection/aktar.py
@@ -4,14 +4,14 @@ from sqlalchemy import create_engine
 # 1. Excel dosyasını oku
 dosya_yolu = "izto_tum_firmalar_temiz.xlsx"
 print("1. Excel dosyası okunuyor, lütfen bekleyin...")
-df = pd.read_excel(r"C:\Users\bulbu\IdeaProjects\StajP1\izto_tum_firmalar_temiz.xlsx", dtype=str)
+df = pd.read_excel(dosya_yolu, dtype=str)
 
 # MySQL'de boşluk ve Türkçe karakterler hata vermesin diye sütun isimlerini temizliyoruz
 df.columns = ['oda_sicil_no', 'ticari_sicil_no', 'meslek_grubu', 'nace_kodu', 'unvani', 'ilce', 'tescilli_adresi', 'web_adresi']
 
 # 2. MySQL Bağlantı Bilgileri (Belirlediğimiz şifre ve veritabanı)
 kullanici = "root"
-sifre = "123456abC" 
+sifre = "change35" 
 host = "127.0.0.1"
 port = "3306"
 veritaban_adi = "staj_db" 
@@ -25,7 +25,7 @@ try:
     
     # Veriyi MySQL'e yaz
     df.to_sql(name=tablo_adi, con=motor, if_exists='replace', index=False)
-    print("3. Harika! Excel'deki tüm firmalar başarıyla MySQL'e aktarıldı. 🚀")
+    print("3. Harika! Excel'deki tüm firmalar başarıyla MySQL'e aktarıldı. ")
 
 except Exception as e:
     print(f"Bir hata oluştu: {e}")
