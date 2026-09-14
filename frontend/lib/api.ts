c08faeb1415c18f2166535806b8c12d35b4fc46e
Taylan Taskin <taylantaskin@yahoo.com>
2026-09-14 17:22:28 +0300

    Add API utility functions for fetching firms, districts, and professions; implement query parameter preparation

---
+++ frontend/lib/api.ts
@@ -0,0 +1,68 @@
+
+const API_BASE_URL = "http://localhost:8080/firmalar";
+
+interface SorguParametreleri{
+    unvani? : string; //unvani: string | undefined yani bu parametre opsiyonel demek "?"
+    meslekGrubuAd?: string;
+    ilceAd?:string;
+    sayfa?: number; //number js deki int ve float gibi sayısal değerleri kapsar
+}
+
+function parametreleriHazirla(params: SorguParametreleri): string{ // java aksine  tipler sağda, ":" dönüş parametresini belirtiyor
+    const query = new URLSearchParams(); // urlsearchParams() js deki url parametrelerini hazırlamak için kullanılan bir sınıf
+    if (params.unvani) { // if : false, 0, NaN, "", null, undefined gibi değerler falsy döner
+    query.set("unvani", params.unvani); 
+    }
+
+    if (params.meslekGrubuAd){
+        query.set("meslekGrubuAd",params.meslekGrubuAd);
+    }
+    
+    if (params.ilceAd){
+        query.set("ilceAd", params.ilceAd);
+    }
+
+    if (params.sayfa ){
+        query.set("sayfa", String(params.sayfa ?? 1));// sayfa parametresi varsa onu kullan, yoksa 1 kullan
+        query.set("sayfaBoyutu", "50");
+    }
+    return query.toString(); //unvani=ABC&ilceAd=Konak
+}
+
+export async function getFirmalar(params: SorguParametreleri){
+    const sorguMetni = parametreleriHazirla(params);
+    const tamUrl = `${API_BASE_URL}?${sorguMetni}`;
+
+    const response = await fetch(tamUrl);
+
+    if (!response.ok){
+        throw new Error ("firma verisi çekilemedi");
+    }
+    const data =await response.json();
+    return data;
+}
+
+export async function getIlceler(){
+    
+    const response = await fetch(`${API_BASE_URL}/ilceler`);
+
+    if (!response.ok){
+        throw new Error ("ilçe verisi çekilemedi");
+    }
+    const data= await response.json();
+
+    return data;
+
+}
+
+export async function getMeslekGruplari(){
+    const response =await fetch (`${API_BASE_URL}/meslek-gruplari`);
+    
+    if(!response.ok){
+        throw new Error ("meslek grubu verisi çekilemedi");
+    }
+    const data= await response.json();
+    return data;
+}
+
+
